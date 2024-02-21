4c5caa79ded8ba99cccc1f99f7971b8574e52cda
ansub <khanansub3@gmail.com>
2024-02-21 22:59:48 +0530

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import { Providers } from '@/app/providers'
+
+import '@/styles/tailwind.css'
+import { type Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s - Protocol API Reference',
+    default: 'Protocol API Reference',
+  },
+}
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className="h-full" suppressHydrationWarning>
+      {children}
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { Header } from '@/components/Header'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      Ansub Khan
+    </div>
+  )
+}
+
+export default Home
