1aa08848d7ed12900eb95ab5bac034196eb23a25
ansub <khanansub3@gmail.com>
2024-02-22 23:32:44 +0530

    Add new dependencies and update tailwind configuration***
    ***Update badges page with table component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -40,7 +40,21 @@ export default {
         7.5: '0.075',
         15: '0.15',
       },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
     },
   },
-  plugins: [typographyPlugin, headlessuiPlugin],
+  plugins: [typographyPlugin, headlessuiPlugin, require('tailwindcss-animate')],
 } satisfies Config
