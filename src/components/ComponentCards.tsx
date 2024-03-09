c48506245166b485299693b431cfc05cb7285962
ansub <khanansub3@gmail.com>
2024-03-09 20:04:42 +0530

    chore: added 2-col grid for smaller devices

--- src/components/ComponentCards.tsx
+++ src/components/ComponentCards.tsx
@@ -44,11 +44,11 @@ const data = [
 const ComponentCards = () => {
   return (
     <div>
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      <div className="grid grid-cols-2 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {data.map((item) => (
           <Link href={item.link} key={item.id}>
             <div className="overflow group rounded-xl border border-white ring-1 ring-zinc-200 transition-all ease-in-out hover:cursor-pointer">
-              <div className="flex h-[12rem] items-center justify-center rounded-t-xl border-b bg-gray-50 text-xs text-gray-400 transition-all ease-in-out group-hover:bg-gray-100">
+              <div className="flex h-[9rem] items-center justify-center rounded-t-xl border-b bg-gray-50 text-xs text-gray-400 transition-all ease-in-out group-hover:bg-gray-100 md:h-[12rem]">
                 <Image
                   width={200}
                   height={200}
