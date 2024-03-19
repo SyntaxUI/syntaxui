d760e4c3cd82c68fa5c565b82aa3fa24d7a9acc8
ansub <khanansub3@gmail.com>
2024-03-19 19:48:39 +0400

    feat: added a new styling to copy code button

--- src/components/CodePreview/CodePreviewComponent.jsx
+++ src/components/CodePreview/CodePreviewComponent.jsx
@@ -22,10 +22,10 @@ function CopyButton({ code }) {
     <button
       type="button"
       className={clsx(
-        'group/button obackdrop-blur absolute right-4 top-3.5 overflow-hidden rounded-full py-1 pl-2 pr-3 text-2xs font-medium transition focus:opacity-100',
+        'group/button obackdrop-blur absolute right-4 top-3.5 overflow-hidden rounded-full   py-1 pl-2 pr-3 text-2xs font-medium transition focus:opacity-100',
         copied
-          ? 'bg-red-400/10 ring-1 ring-inset ring-red-400/20'
-          : 'bg-white/5 hover:bg-white/7.5 dark:bg-white/2.5 dark:hover:bg-white/5',
+          ? 'border border-red-500 bg-red-400/10 ring-1 ring-inset ring-red-400/20'
+          : 'border border-gray-600 bg-white/5 hover:bg-white/7.5 dark:bg-white/2.5 dark:hover:bg-white/5',
       )}
       onClick={() => {
         window.navigator.clipboard.writeText(code).then(() => {
@@ -36,21 +36,21 @@ function CopyButton({ code }) {
       <span
         aria-hidden={copied}
         className={clsx(
-          'pointer-events-none flex items-center gap-0.5 text-zinc-400 transition duration-300',
+          'pointer-events-none flex items-center  gap-1 text-zinc-400 transition duration-300',
           copied && '-translate-y-1.5 opacity-0',
         )}
       >
-        <ClipboardIcon className="h-5 w-5 fill-zinc-500/20 stroke-zinc-500 transition-colors group-hover/button:stroke-zinc-400" />
-        Copy
+        <ClipboardIcon className="h-4 w-4  fill-zinc-500/20 stroke-zinc-500 transition-colors group-hover/button:stroke-zinc-400" />
+        Copy Code
       </span>
       <span
         aria-hidden={!copied}
         className={clsx(
-          'pointer-events-none absolute inset-0 flex items-center justify-center text-red-400 transition duration-300',
+          'pointer-events-none absolute inset-0 flex items-center justify-center  text-red-400 transition duration-300',
           !copied && 'translate-y-1.5 opacity-0',
         )}
       >
-        Copied!
+        Code Copied!
       </span>
     </button>
   )
