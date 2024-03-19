c1c4812c7b2ec227d20983bcad07193edb51718b
ansub <khanansub3@gmail.com>
2024-03-19 19:40:53 +0400

    feat: added icons for preview and code tab in CodePreviewComponent

--- src/components/CodePreview/CodePreviewComponent.jsx
+++ src/components/CodePreview/CodePreviewComponent.jsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from 'react'
 import { ClipboardIcon } from '../icons/ClipboardIcon'
 import clsx from 'clsx'
+import { Code, Eye } from 'lucide-react'
 
 function CopyButton({ code }) {
   let [copyCount, setCopyCount] = useState(0)
@@ -65,25 +66,33 @@ const CodePreviewComponent = ({ previewContent, highlighted, code }) => {
   return (
     <div>
       <div className="border-b border-gray-200">
-        <nav className="-mb-px flex space-x-8">
+        <nav className="-mb-px flex space-x-5">
           <button
             className={`${
               activeTab === 'preview'
-                ? 'border-red-500 text-red-600'
+                ? 'border-red-500 text-black'
                 : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'
-            } whitespace-nowrap border-b-2 px-1 py-4 text-sm font-medium`}
+            } flex items-center gap-1 whitespace-nowrap border-b-2 px-1 py-2 text-sm font-medium`}
             onClick={() => handleTabClick('preview')}
           >
+            <Eye
+              size={14}
+              className={activeTab === 'preview' ? 'text-red-500' : ''}
+            />
             Preview
           </button>
           <button
             className={`${
               activeTab === 'code'
-                ? 'border-red-500 text-red-600'
+                ? 'border-red-500 text-black'
                 : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'
-            } whitespace-nowrap border-b-2 px-1 py-4 text-sm font-medium`}
+            } flex items-center gap-1 whitespace-nowrap border-b-2 px-1 py-2 text-sm font-medium`}
             onClick={() => handleTabClick('code')}
           >
+            <Code
+              size={14}
+              className={activeTab === 'code' ? 'text-red-500' : ''}
+            />{' '}
             Code
           </button>
         </nav>
