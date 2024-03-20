4ec7cc02ad94ad0195452d1a887110a4272d014f
ansub <khanansub3@gmail.com>
2024-03-20 16:21:21 +0400

    refactor: changed the vibrate animation translate

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -135,31 +135,31 @@ export default {
             transform: 'translateY(0)',
           },
           '10%': {
-            transform: 'translateY(-4px)',
+            transform: 'translateY(-3px)',
           },
           '20%': {
             transform: 'translateY(0px)',
           },
           '30%': {
-            transform: 'translateY(4px)',
+            transform: 'translateY(3px)',
           },
           '40%': {
             transform: 'translateY(0px)',
           },
           '50%': {
-            transform: 'translateY(-4px)',
+            transform: 'translateY(-3px)',
           },
           '60%': {
             transform: 'translateY(0px)',
           },
           '70%': {
-            transform: 'translateY(4px)',
+            transform: 'translateY(3px)',
           },
           '80%': {
             transform: 'translateY(0px)',
           },
           '90%': {
-            transform: 'translateY(-4px)',
+            transform: 'translateY(-3px)',
           },
           '100%': {
             transform: 'translateY(0)',
