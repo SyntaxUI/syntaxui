452edb4213765e0091689242ca97583b1cb6a2ca
mostri28 <mateoetchepare@gmail.com>
2024-03-29 18:26:13 -0300

    FIX - Issue #15
    just a tailwind property that did not belong there.

--- src/ui/Loader.tsx
+++ src/ui/Loader.tsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion'
 
 export const BounceLoader = () => {
   return (
-    <div className="flex items-center justify-center space-x-2 bg-white">
+    <div className="flex items-center justify-center space-x-2">
       <div className="h-5 w-5 animate-bounce rounded-full bg-red-500 [animation-delay:-0.3s]"></div>
       <div className="h-5 w-5 animate-bounce rounded-full bg-red-500 [animation-delay:-0.13s]"></div>
       <div className="h-5 w-5 animate-bounce rounded-full bg-red-500"></div>
