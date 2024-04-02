7f183ba2599796dce1a6ffd54f28ca188dc2889a
ansub <khanansub3@gmail.com>
2024-04-02 14:21:51 +0530

    fix: transition effect on navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -112,7 +112,7 @@ function VisibleSectionHighlight({
     <motion.div
       layout
       initial={{ opacity: 0 }}
-      animate={{ opacity: 1, transition: { delay: 0.2 } }}
+      animate={{ opacity: 1 }}
       transition={{ duration: 0.15 }}
       exit={{ opacity: 0 }}
       className="absolute inset-x-0 top-0 bg-zinc-800/2.5 will-change-transform dark:bg-white/2.5"
@@ -138,7 +138,7 @@ function ActivePageMarker({
       layout
       className="absolute left-2 h-6 w-px bg-red-500"
       initial={{ opacity: 0 }}
-      animate={{ opacity: 1, transition: { delay: 0.2 } }}
+      animate={{ opacity: 1 }}
       transition={{ duration: 0.15 }}
       exit={{ opacity: 0 }}
       style={{ top }}
