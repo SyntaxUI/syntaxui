5adbe95c64cf27239959e42803726285abee84c1
ansub <khanansub3@gmail.com>
2024-04-09 19:25:43 +0530

    style: changed the color of text ticker component

--- src/components/PreviewCode/Text.tsx
+++ src/components/PreviewCode/Text.tsx
@@ -2,7 +2,7 @@ import Counter from '../Counter'
 
 export const TextTicker = () => {
   return (
-    <div className="text-2xl font-semibold tracking-tight">
+    <div className="text-4xl font-semibold tracking-tight text-red-500">
       <Counter value={1000} />
     </div>
   )
