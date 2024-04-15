5398230f207729bc460a1865c5d675a621710707
ansub <khanansub3@gmail.com>
2024-04-16 00:17:28 +0530

    feat: added button cards for 3d and heartbeat button

--- src/ui/animation/HeartbeatButton.tsx
+++ src/ui/animation/HeartbeatButton.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const HeartbeatButton = () => {
   return (
     <div>
-      <button className="m-9 animate-buttonheartbeat rounded-md bg-red-500 px-6 py-2 text-sm font-semibold text-white">
+      <button className="m-9 animate-buttonheartbeat rounded-md bg-red-500 px-6 py-1 text-sm font-semibold text-white">
         Ansub
       </button>
     </div>
