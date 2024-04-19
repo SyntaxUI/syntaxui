e70145f872df02213ee8ad40e9b1e8089040ca3d
Saud <viasaud@gmail.com>
2024-04-19 20:55:46 +0300

    Style: Fixed an underline shown on the buttons on the docs page

--- src/components/HoverAnimationCards.tsx
+++ src/components/HoverAnimationCards.tsx
@@ -38,7 +38,7 @@ const HoverAnimationCards = () => {
     <div>
       <div className="grid grid-cols-2 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {data.map((item) => (
-          <Link href={item.link} key={item.id}>
+          <Link href={item.link} key={item.id} className="no-underline">
             <div className="overflow group group rounded-xl border border-white ring-1 ring-zinc-200 transition-all ease-in-out hover:cursor-pointer">
               <div className="flex h-[9rem] items-center justify-center overflow-hidden rounded-t-xl border-b bg-gray-50 text-xs text-gray-400 transition-all ease-in-out group-hover:bg-gray-100 md:h-[12rem]">
                 {item.component}
