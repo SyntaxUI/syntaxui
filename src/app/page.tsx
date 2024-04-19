068db008c1b727f34c12f28eb1458c9a5af9743c
ansub <khanansub3@gmail.com>
2024-04-20 01:10:18 +0530

    feat: add join discord button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ import { Button } from '@/components/ui/button'
 import GitHubIcon from '@/icons/github'
 import Image from 'next/image'
 import Link from 'next/link'
+import { motion } from 'framer-motion'
 
 const Home = () => {
   return (
@@ -18,6 +19,37 @@ const Home = () => {
         <Header />
         <div className="my-[8rem] flex h-full flex-col items-center justify-center gap-4 px-3">
           <div className="flex w-full flex-col items-center justify-center gap-2">
+            <motion.button
+              className="relative mb-8 inline-flex items-center gap-2 overflow-hidden rounded-full bg-blue-50 px-3 py-1 text-xs font-medium text-[#5865F2] ring-1 ring-inset ring-blue-700/10"
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 0.5 }}
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              //  add spring animation
+              onClick={() => {
+                window.open('https://discord.gg/P8GXYyH3ZU', '_blank')
+              }}
+            >
+              Join the Discord Family
+              <svg
+                viewBox="0 0 256 199"
+                width="16"
+                height="16"
+                xmlns="http://www.w3.org/2000/svg"
+                preserveAspectRatio="xMidYMid"
+              >
+                <path
+                  d="M216.856 16.597A208.502 208.502 0 0 0 164.042 0c-2.275 4.113-4.933 9.645-6.766 14.046-19.692-2.961-39.203-2.961-58.533 0-1.832-4.4-4.55-9.933-6.846-14.046a207.809 207.809 0 0 0-52.855 16.638C5.618 67.147-3.443 116.4 1.087 164.956c22.169 16.555 43.653 26.612 64.775 33.193A161.094 161.094 0 0 0 79.735 175.3a136.413 136.413 0 0 1-21.846-10.632 108.636 108.636 0 0 0 5.356-4.237c42.122 19.702 87.89 19.702 129.51 0a131.66 131.66 0 0 0 5.355 4.237 136.07 136.07 0 0 1-21.886 10.653c4.006 8.02 8.638 15.67 13.873 22.848 21.142-6.58 42.646-16.637 64.815-33.213 5.316-56.288-9.08-105.09-38.056-148.36ZM85.474 135.095c-12.645 0-23.015-11.805-23.015-26.18s10.149-26.2 23.015-26.2c12.867 0 23.236 11.804 23.015 26.2.02 14.375-10.148 26.18-23.015 26.18Zm85.051 0c-12.645 0-23.014-11.805-23.014-26.18s10.148-26.2 23.014-26.2c12.867 0 23.236 11.804 23.015 26.2 0 14.375-10.148 26.18-23.015 26.18Z"
+                  fill="#5865F2"
+                />
+              </svg>
+              <motion.div
+                className="absolute inset-0 bg-gradient-to-r from-blue-500 to-purple-500 opacity-30 blur-lg"
+                animate={{ rotate: 360 }}
+                transition={{ duration: 5, repeat: Infinity, ease: 'linear' }}
+              ></motion.div>
+            </motion.button>
             <Image
               src="/images/syntaxUI.svg"
               alt="syntaxUI"
