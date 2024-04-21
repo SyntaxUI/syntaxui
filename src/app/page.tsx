636c0d46658d4b78b031d6ba333686b4926891a0
ansub <khanansub3@gmail.com>
2024-04-21 12:56:32 +0530

    chore: removed comment from discord logo in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,6 @@ const Home = () => {
               transition={{ duration: 0.5 }}
               whileHover={{ scale: 1.1 }}
               whileTap={{ scale: 0.9 }}
-              //  add spring animation
               onClick={() => {
                 window.open('https://discord.gg/P8GXYyH3ZU', '_blank')
               }}
