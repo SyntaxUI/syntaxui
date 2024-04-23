20665094524127106d01aa6baaccba32ab9e451a
Saud <viasaud@gmail.com>
2024-04-23 19:43:36 +0300

    feat: Added Stitches Button
    * feat: Added Stitches Button to ButtonCards.tsx
    
    * Refactor StitchesButton import paths in multiple files

--- src/components/Cards/ButtonCards.tsx
+++ src/components/Cards/ButtonCards.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import { ThreeDButton } from '@/ui/button'
 import HeartbeatButton from '@/ui/animation/HeartbeatButton'
 import { NeubrutalismButton } from '../CodePreview/Buttons'
+import StitchesButton from '@/components/CodePreview/stitches-button'
 
 const data = [
   {
@@ -17,10 +18,16 @@ const data = [
     component: <NeubrutalismButton />,
   },
   {
-    id: 2,
+    id: 3,
     title: 'Heartbeat Button',
     link: '/docs/components/button/heartbeat-button',
     component: <HeartbeatButton />,
+  },
+  {
+    id: 4,
+    title: 'Stitches Button',
+    link: '/docs/components/button/stitches-button',
+    component: <StitchesButton />,
   },
 ]
 
@@ -30,7 +37,7 @@ const ButtonCards = () => {
       <div className="grid grid-cols-2 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {data.map((item) => (
           <Link href={item.link} key={item.id} className="no-underline">
-            <div className="overflow group group rounded-xl border border-white ring-1 ring-zinc-200 transition-all ease-in-out hover:cursor-pointer">
+            <div className="overflow group rounded-xl border border-white ring-1 ring-zinc-200 transition-all ease-in-out hover:cursor-pointer">
               <div className="flex h-[9rem] items-center justify-center overflow-hidden rounded-t-xl border-b bg-gray-50 text-xs text-gray-400 transition-all ease-in-out group-hover:bg-gray-100 md:h-[12rem]">
                 {item.component}
               </div>
