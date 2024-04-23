2161903e70951544ded4a390d2aff2ea91472772
Saud <viasaud@gmail.com>
2024-04-23 19:22:58 +0300

    Refactor StitchesButton import paths in multiple files

--- src/components/Cards/ButtonCards.tsx
+++ src/components/Cards/ButtonCards.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import { ThreeDButton } from '@/ui/button'
 import HeartbeatButton from '@/ui/animation/HeartbeatButton'
 import { NeubrutalismButton } from '../CodePreview/Buttons'
-import StitchesButton from '@/ui/stitches-button'
+import StitchesButton from '@/components/CodePreview/stitches-button'
 
 const data = [
   {
