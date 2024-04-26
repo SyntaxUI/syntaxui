add8067ce6a1ab185769677f7cf8c56bea8ed7b0
Ethan Pollack <epollack31@gmail.com>
2024-04-26 15:06:05 -0700

    feat: Added a Neobrutalism Toggle (#94)

--- src/components/cards/ToggleCards.tsx
+++ src/components/cards/ToggleCards.tsx
@@ -1,21 +1,32 @@
-import SimpleToggle from '@/showcase/components/toggle/SimpleToggle'
-import Link from 'next/link'
+import NeobrutalismToggle from '@/showcase/components/toggle/NeobrutalismToggle';
+import SimpleToggle from '@/showcase/components/toggle/SimpleToggle';
+import Link from 'next/link';
 
-const data = [
+interface ToggleCardProps {
+  title: string;
+  link: string;
+  component: React.JSX.Element;
+  hide?: boolean;
+}
+const data: ToggleCardProps[] = [
   {
-    id: 1,
     title: 'Simple Toggle',
     link: '/docs/components/toggle/simple-toggle',
     component: <SimpleToggle />,
   },
-]
+  {
+    title: 'Neobrutalism Toggle',
+    link: '/docs/components/toggle/neobrutalism-toggle',
+    component: <NeobrutalismToggle />,
+  },
+];
 
 const ButtonCards = () => {
   return (
     <div>
       <div className="grid grid-cols-2 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {data.map((item) => (
-          <Link href={item.link} key={item.id} className="no-underline">
+        {data.map((item, i) => (
+          <Link href={item.link} key={i} className="no-underline">
             <div className="overflow group group rounded-xl border border-white ring-1 ring-zinc-200 transition-all ease-in-out hover:cursor-pointer">
               <div className="flex h-[9rem] items-center justify-center overflow-hidden rounded-t-xl border-b bg-gray-50 text-xs text-gray-400 transition-all ease-in-out group-hover:bg-gray-100 md:h-[12rem]">
                 {item.component}
@@ -28,7 +39,7 @@ const ButtonCards = () => {
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ButtonCards
+export default ButtonCards;
