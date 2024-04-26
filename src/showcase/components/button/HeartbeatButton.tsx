814d83f12cbe06ad6f8365a24b07a1ffa692cf3a
Ethan Pollack <epollack31@gmail.com>
2024-04-25 21:29:35 -0700

    chore: updated copy of HeartbeatButton (#80)

--- src/showcase/components/button/HeartbeatButton.tsx
+++ src/showcase/components/button/HeartbeatButton.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 const HeartbeatButton = () => {
   return (
     <div>
-      <button className="m-9 animate-buttonheartbeat rounded-md bg-red-500 px-6 py-1 text-sm font-semibold text-white">
-        Ansub
+      <button className="m-9 animate-buttonheartbeat rounded-md bg-red-500 px-4 py-1 text-sm font-semibold text-white">
+        Syntax UI
       </button>
     </div>
   )
