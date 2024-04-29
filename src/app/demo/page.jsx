9e708dd7a6b75fd9c9507fa39a0b190ac8465ee7
ethan <epollack31@gmail.com>
2024-04-29 11:21:10 -0700

    removed ComponentPreview from demo

--- src/app/demo/page.jsx
+++ src/app/demo/page.jsx
@@ -1,13 +1,13 @@
 'use client'
 
 import React from 'react'
-import { ComponentPreview } from '@/components/code/ComponentPreview'
+// import { ComponentPreview } from '@/components/code/ComponentPreview'
 
 export default function Demo() {
   return (
     <div className="flex h-screen w-full items-center justify-center">
       <div className="h-screen w-full max-w-4xl bg-white">
-        <ComponentPreview path="components/toggle/SimpleToggle" />{' '}
+        {/* <ComponentPreview path="components/toggle/SimpleToggle" />{' '} */}
       </div>
     </div>
   )
