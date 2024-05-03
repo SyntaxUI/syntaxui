ac2a43ce61c5ada9b01e4e039f4907349effc19c
Shriprasanna <shriprasanna32@gmail.com>
2024-05-04 01:17:45 +0530

    fix : hover animation

--- src/showcase/components/button/ShineButton.tsx
+++ src/showcase/components/button/ShineButton.tsx
@@ -4,9 +4,9 @@ import React from 'react'
 
 const ShineButton = () => {
   return (
-    <button className="group relative inline-flex items-center justify-center overflow-hidden rounded-md bg-red-500 px-4 py-1.5 text-xs font-normal text-white transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-lg hover:shadow-red-500/30">
+    <button className="group/button relative inline-flex items-center justify-center overflow-hidden rounded-md bg-red-500 px-4 py-1.5 text-xs font-normal text-white transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-lg hover:shadow-red-500/30">
       <span className="text-sm">Syntax UI</span>
-      <div className="absolute inset-0 flex h-full w-full justify-center [transform:skew(-13deg)_translateX(-100%)] group-hover:duration-1000 group-hover:[transform:skew(-13deg)_translateX(100%)]">
+      <div className="absolute inset-0 flex h-full w-full justify-center [transform:skew(-13deg)_translateX(-100%)] group-hover/button:duration-1000 group-hover/button:[transform:skew(-13deg)_translateX(100%)]">
         <div className="relative h-full w-8 bg-white/20" />
       </div>
     </button>
