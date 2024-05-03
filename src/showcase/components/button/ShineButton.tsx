2f629f18b33d8744e070d654270392f603f91761
Shriprasanna <shriprasanna32@gmail.com>
2024-05-03 21:03:17 +0530

    style : change in button color

--- src/showcase/components/button/ShineButton.tsx
+++ src/showcase/components/button/ShineButton.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const ShineButton = () => {
   return (
-    <button className="group relative inline-flex items-center justify-center overflow-hidden rounded-md bg-[#FF1059] px-4 py-1.5 text-xs font-normal text-white transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-lg hover:shadow-[#F43753]/30">
+    <button className="group relative inline-flex items-center justify-center overflow-hidden rounded-md bg-red-500 px-4 py-1.5 text-xs font-normal text-white transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-lg hover:shadow-red-500/30">
       <span className="text-sm">Syntax UI</span>
       <div className="absolute inset-0 flex h-full w-full justify-center [transform:skew(-13deg)_translateX(-100%)] group-hover:duration-1000 group-hover:[transform:skew(-13deg)_translateX(100%)]">
         <div className="relative h-full w-8 bg-white/20" />
