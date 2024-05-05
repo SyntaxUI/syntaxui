de66f78cd2e51b22c772014526e1fca5696f15e2
Dominik Koch <dominik@koch-bautechnik.de>
2024-05-05 20:25:59 +0200

    fix: center on all browsers

--- src/components/code/CodeGroup.tsx
+++ src/components/code/CodeGroup.tsx
@@ -52,7 +52,7 @@ export default function CodeGroup({
         <div ref={codeRef}>{children}</div>
         {/* {canExpand && ( */}
         <a
-          className="w-xs absolute bottom-0 left-0 right-0 block rounded-2xl bg-transparent p-0 text-sm text-center font-medium text-white no-underline cursor-pointer"
+          className="w-full absolute bottom-0 left-0 right-0 block rounded-2xl bg-transparent p-0 text-sm font-medium text-white no-underline cursor-pointer"
           onClick={() => setMinimized(!minimized)}
         >
           {minimized ? 'Expand' : 'Collapse'}
