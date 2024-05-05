33bff11e4d3ab8b428d5643ef033c45c41cede55
ethan <epollack31@gmail.com>
2024-05-04 22:31:30 -0400

    added border glow

---
+++ src/showcase/effects/BorderGlow.tsx
@@ -0,0 +1,46 @@
+'use client'
+
+import { ReactNode, useEffect, useRef } from 'react'
+
+const BorderGlow = ({ children }: { children: ReactNode }) => {
+  const ref = useRef<HTMLDivElement>(null)
+  const id = useRef<string>(Math.random().toString(36).substring(2, 9))
+
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      if (!ref.current) return
+
+      const rect = ref.current.getBoundingClientRect()
+      const x = e.clientX - rect.left
+      const y = e.clientY - rect.top
+      document.documentElement.style.setProperty(`--x-${id.current}`, `${x}px`)
+      document.documentElement.style.setProperty(`--y-${id.current}`, `${y}px`)
+    }
+    document.addEventListener('mousemove', handleMouseMove)
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove)
+    }
+  }, [ref.current])
+
+  return (
+    <div
+      className="relative overflow-hidden rounded-xl border bg-white"
+      ref={ref}
+      style={
+        {
+          '--x': `var(--x-${id.current})`,
+          '--y': `var(--y-${id.current})`,
+        } as any
+      }
+    >
+      <span
+        className={`absolute left-[var(--x)] top-[var(--y)] z-0 h-36 w-36 -translate-x-1/2 -translate-y-1/2 bg-[radial-gradient(#fb3b53_0%,transparent_70%)]`}
+      ></span>
+      <div className="relative z-10 m-[1px] rounded-[calc(0.75rem-1px)] bg-white px-6 py-2">
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default BorderGlow
--- src/showcase/ui-group/EffectCards.tsx
+++ src/showcase/ui-group/EffectCards.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import BorderGlow from '@/showcase/effects/BorderGlow'
 
 const data = [
   {
@@ -8,6 +9,17 @@ const data = [
     title: 'Image Fade',
     image: '/images/ui/image-fade.png',
     link: '/docs/effects/image-fade',
+  },
+  {
+    id: 2,
+    title: 'Border Glow',
+    preview: (
+      <div className="flex flex-col gap-2">
+        <BorderGlow>Border Glow</BorderGlow>
+        <BorderGlow>Border Glow</BorderGlow>
+      </div>
+    ),
+    link: '/docs/effects/border-glow',
   },
 ]
 
@@ -19,13 +31,16 @@ const EffectCards = () => {
           <Link href={item.link} key={item.id}>
             <div className="overflow group rounded-xl border border-white ring-1 ring-zinc-200 transition-all ease-in-out hover:cursor-pointer">
               <div className="flex h-[9rem] items-center justify-center rounded-t-xl border-b bg-gray-50 text-xs text-gray-400 transition-all ease-in-out group-hover:bg-gray-100 md:h-[12rem]">
-                <Image
-                  width={200}
-                  height={200}
-                  src={item.image}
-                  alt={item.title}
-                  className="h-auto w-[150px] duration-300 ease-in-out group-hover:scale-110"
-                />
+                {item.preview ? item.preview : null}
+                {item.image ? (
+                  <Image
+                    width={200}
+                    height={200}
+                    src={item.image}
+                    alt={item.title}
+                    className="h-auto w-[150px] duration-300 ease-in-out group-hover:scale-110"
+                  />
+                ) : null}
               </div>
               <div className="w-full p-4 text-sm font-medium text-gray-800">
                 {item.title}
