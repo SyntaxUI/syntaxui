bab6468a04c5732a1be4c8c3cd96c77603b36702
ethan <epollack31@gmail.com>
2024-05-05 12:40:32 -0400

    fixed border styling

--- src/showcase/effects/BorderGlow.tsx
+++ src/showcase/effects/BorderGlow.tsx
@@ -3,6 +3,7 @@
 import { ReactNode, useEffect, useRef } from 'react'
 
 const BorderGlow = ({ children }: { children: ReactNode }) => {
+  // TODO: fix default position
   const ref = useRef<HTMLDivElement>(null)
   const id = useRef<string>(Math.random().toString(36).substring(2, 9))
 
@@ -22,9 +23,14 @@ const BorderGlow = ({ children }: { children: ReactNode }) => {
     }
   }, [ref.current])
 
+  useEffect(() => {
+    document.documentElement.style.setProperty(`--x-${id.current}`, `-100%`)
+    document.documentElement.style.setProperty(`--y-${id.current}`, `-100%`)
+  }, [])
+
   return (
     <div
-      className="relative overflow-hidden rounded-xl border bg-white"
+      className="relative overflow-hidden rounded-xl bg-[#e5e7eb]"
       ref={ref}
       style={
         {
@@ -34,7 +40,7 @@ const BorderGlow = ({ children }: { children: ReactNode }) => {
       }
     >
       <span
-        className={`absolute left-[var(--x)] top-[var(--y)] z-0 h-36 w-36 -translate-x-1/2 -translate-y-1/2 bg-[radial-gradient(#fb3b53_0%,transparent_70%)]`}
+        className={`absolute left-[var(--x)] top-[var(--y)] z-0 h-36 w-36 -translate-x-1/2 -translate-y-1/2  bg-[radial-gradient(#fb3b53_0%,transparent_70%)]`}
       ></span>
       <div className="relative z-10 m-[1px] rounded-[calc(0.75rem-1px)] bg-white px-6 py-2">
         {children}
