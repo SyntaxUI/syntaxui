b3faccdbdfaaecd06a60127401819245b85d39c7
vanshavenger <vanshchopra101@gmail.com>
2024-05-14 02:33:48 +0530

    [Enhancement]:Dark mode initialising - Flickering problem solved

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider, useTheme } from 'next-themes'
 
 import posthog from 'posthog-js'
 import { PostHogProvider } from 'posthog-js/react'
+import { type ThemeProviderProps } from 'next-themes/dist/types'
 
 if (typeof window !== 'undefined') {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY ?? '', {
@@ -15,34 +16,34 @@ function CSPostHogProvider({ children }: { children: React.ReactNode }) {
   return <PostHogProvider client={posthog}>{children}</PostHogProvider>
 }
 
-function ThemeWatcher() {
-  let { resolvedTheme, setTheme } = useTheme()
+// function ThemeWatcher() {
+//   let { resolvedTheme, setTheme } = useTheme()
 
-  useEffect(() => {
-    let media = window.matchMedia('(prefers-color-scheme: dark)')
+//   useEffect(() => {
+//     let media = window.matchMedia('(prefers-color-scheme: dark)')
 
-    function onMediaChange() {
-      let systemTheme = media.matches ? 'dark' : 'light'
-      if (resolvedTheme === systemTheme) {
-        setTheme('light')
-      }
-    }
+//     function onMediaChange() {
+//       let systemTheme = media.matches ? 'dark' : 'light'
+//       if (resolvedTheme === systemTheme) {
+//         setTheme('light')
+//       }
+//     }
 
-    onMediaChange()
-    media.addEventListener('change', onMediaChange)
+//     onMediaChange()
+//     media.addEventListener('change', onMediaChange)
 
-    return () => {
-      media.removeEventListener('change', onMediaChange)
-    }
-  }, [resolvedTheme, setTheme])
+//     return () => {
+//       media.removeEventListener('change', onMediaChange)
+//     }
+//   }, [resolvedTheme, setTheme])
 
-  return null
-}
+//   return null
+// }
 
-export function Providers({ children }: { children: React.ReactNode }) {
+export function Providers({ children, ...props }: ThemeProviderProps) {
   return (
-    <ThemeProvider attribute="class" disableTransitionOnChange>
-      <ThemeWatcher />
+    <ThemeProvider {...props} attribute="class" disableTransitionOnChange>
+      {/* <ThemeWatcher /> */}
       <CSPostHogProvider>{children}</CSPostHogProvider>
     </ThemeProvider>
   )
