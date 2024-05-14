3031380c37f4e64d63210af41cc53ef3af1e08c4
ansub <khanansub3@gmail.com>
2024-05-14 19:42:45 +0530

    add Dark Mode colors

--- src/showcase/ui-group/ComponentCards.tsx
+++ src/showcase/ui-group/ComponentCards.tsx
@@ -70,7 +70,7 @@ const ComponentCards = () => {
       <div className="grid grid-cols-2 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {data.map((item, index) => (
           <Link href={item.link} key={index} className="no-underline">
-            <div className="overflow group rounded-xl border border-white ring-1 ring-zinc-200 transition-all ease-in-out hover:cursor-pointer">
+            <div className="overflow group rounded-xl border border-white ring-1 ring-zinc-200 transition-all ease-in-out hover:cursor-pointer dark:border-gray-600 dark:ring-gray-600">
               <div className="flex h-[9rem] items-center justify-center overflow-hidden rounded-t-xl border-b bg-gray-50 text-xs text-gray-400 transition-all ease-in-out group-hover:bg-gray-100 md:h-[12rem]">
                 {item.image ? (
                   <Image
