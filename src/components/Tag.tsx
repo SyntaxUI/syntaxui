f7cf5443d4500f9bf60e1a3394a69a584e2aa4c4
Ansub <khanansub3@gmail.com>
2024-05-16 23:51:46 +0530

    ✨ added new badge in navigation (#194)

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -1,10 +1,25 @@
 import clsx from 'clsx'
 
-export function Tag({ children }: { children: string }) {
+const variantStyles = {
+  small: '',
+  medium: 'rounded-lg px-1.5 ring-1 ring-inset',
+}
+
+export function Tag({
+  children,
+  variant = 'medium',
+}: {
+  children: string
+  variant?: keyof typeof variantStyles
+}) {
   return (
     <span
       className={clsx(
-        'text-[0.625rem] font-semibold uppercase text-red-500 ring-red-300  dark:text-red-400 dark:ring-red-400/30',
+        'text-[0.625rem] font-semibold uppercase',
+        'bg-text-200 text-red-500 dark:text-red-400',
+        variantStyles[variant],
+        variant === 'medium' &&
+          'bg-red-400/10 ring-red-300 dark:ring-red-400/30',
       )}
     >
       {children}
