55c020ceb297a40cf9d149fd731cb736989f07aa
Subhadeep Roy <subha9.5roy350@gmail.com>
2024-05-19 23:59:29 +0530

    Feat: Added Four Column Footer

---
+++ src/showcase/components/footer/FourColumnFooter.tsx
@@ -0,0 +1,167 @@
+import Image from 'next/image'
+
+const navigation = {
+  connect: [
+    { name: 'Book Meeting', href: '' },
+    {
+      name: 'Twitter',
+      href: 'https://twitter.com/justansub',
+    },
+    {
+      name: 'Github',
+      href: 'https://www.youtube.com/@SpeedyBrand-SEO',
+    },
+    {
+      name: 'LinkedIn',
+      href: 'https://www.linkedin.com/company/speedy-brand-inc/',
+    },
+  ],
+  company: [
+    { name: 'Blogs', href: '/' },
+    { name: 'Pricing', href: '/' },
+    { name: 'Affiliate Partner', href: '/' },
+    { name: 'AI For Enterprise', href: '/' },
+  ],
+
+  quick: [
+    { name: 'About', href: '/' },
+    { name: 'Team', href: '/' },
+    { name: 'Contact', href: '/' },
+    { name: 'Issues', href: '/' },
+  ],
+
+  legal: [
+    { name: 'Privacy', href: '/' },
+    { name: 'Terms', href: '/' },
+    { name: 'COC', href: '/' },
+    { name: 'License', href: '/' },
+  ],
+}
+
+const FourColumnFooter = () => {
+  return (
+    <footer
+      aria-labelledby="footer-heading"
+      className="font-inter w-full max-w-7xl"
+    >
+      <h2 id="footer-heading" className="sr-only">
+        Footer
+      </h2>
+      <div className="mx-auto max-w-7xl px-2">
+        <div className="flex flex-col justify-between lg:flex-row">
+          <div className="space-y-8">
+            <div className="flex items-center gap-2">
+              <Image
+                priority={true}
+                unoptimized={true}
+                width={100}
+                height={40}
+                src="/images/syntaxUI.svg"
+                alt="logo"
+                className="h-7 w-auto"
+              />
+              <h1 className="text-2xl my-auto">
+                SyntaxUI
+              </h1>
+            </div>
+            <p className="text-md mt-0 max-w-xs leading-6 text-gray-700">
+              Not your average component library - build faster, launch sooner.
+            </p>
+            <div className="flex space-x-6 text-sm text-gray-700">
+              <div>Made with ❤️ by Ansub.</div>
+            </div>
+          </div>
+          {/* Navigations */}
+          <div className="grid grid-cols-2 gap-14 gap-y-4 md:grid-cols-4 lg:mt-0 lg:grid-cols-4 xl:col-span-2">
+            <div className="md:mt-0">
+              <h3 className="text-sm font-semibold leading-6 text-gray-900">
+                Connect
+              </h3>
+              <div className="mt-6 space-y-4">
+                {navigation.connect.map((item) => (
+                  <div key={item.name}>
+                    <a
+                      href={item.href}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="text-sm leading-6 text-gray-700 hover:text-gray-900"
+                    >
+                      {item.name}
+                    </a>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div>
+              <div>
+                <h3 className="text-sm font-semibold leading-6 text-gray-900">
+                  Company
+                </h3>
+                <div className="mt-6 space-y-4">
+                  {navigation.company.map((item) => (
+                    <div key={item.name}>
+                      <a
+                        href={item.href}
+                        className="text-sm leading-6 text-gray-700 hover:text-gray-900"
+                      >
+                        {item.name}
+                      </a>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+            <div className="md:mt-0">
+              <h3 className="text-sm font-semibold leading-6 text-gray-900">
+                Quick Links
+              </h3>
+              <div className="mt-6 space-y-4">
+                {navigation.quick.map((item) => (
+                  <div key={item.name}>
+                    <a
+                      href={item.href}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="text-sm leading-6 text-gray-700 hover:text-gray-900"
+                    >
+                      {item.name}
+                    </a>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div>
+              <div>
+                <h3 className="text-sm font-semibold leading-6 text-gray-900">
+                  Legal
+                </h3>
+                <div className="mt-6 space-y-4">
+                  {navigation.legal.map((item) => (
+                    <div key={item.name}>
+                      <a
+                        href={item.href}
+                        className="text-sm leading-6 text-gray-700 hover:text-gray-900"
+                      >
+                        {item.name}
+                      </a>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="mt-8 border-t border-gray-900/10 pt-4 sm:mt-10 lg:mt-12">
+          <p className="text-xs leading-5 text-gray-700">
+            &copy; 
+            {
+              new Date().getFullYear()
+            } {' '} SyntaxUI. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default FourColumnFooter
