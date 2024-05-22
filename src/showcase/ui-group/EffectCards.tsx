333cfcbb83af538ff3af54fd15f8c971021a0e47
ethan <epollack31@gmail.com>
2024-05-22 09:10:59 -0700

    fixed docs page / updated login in EffectCards

--- src/showcase/ui-group/EffectCards.tsx
+++ src/showcase/ui-group/EffectCards.tsx
@@ -39,7 +39,7 @@ const EffectCards = () => {
           <Link href={item.link} key={item.id}>
             <div className="overflow group rounded-xl border border-white ring-1 ring-zinc-200 transition-all ease-in-out hover:cursor-pointer">
               <div className="flex h-[9rem] items-center justify-center overflow-hidden rounded-t-xl border-b bg-gray-50 text-xs text-gray-400 transition-all ease-in-out group-hover:bg-gray-100 md:h-[12rem]">
-                {item.image && (
+                {item.image ? (
                   <Image
                     width={200}
                     height={200}
@@ -47,8 +47,9 @@ const EffectCards = () => {
                     alt={item.title}
                     className="h-auto w-[150px] duration-300 ease-in-out group-hover:scale-110"
                   />
+                ) : (
+                  item.preview
                 )}
-                {item.preview}
               </div>
               <div className="w-full p-4 text-sm font-medium text-gray-800">
                 {item.title}
