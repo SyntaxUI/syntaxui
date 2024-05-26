927027ecda607cb372848d8c248ea91a1b4379ed
Ansub <khanansub3@gmail.com>
2024-05-26 17:22:16 +0400

    ✨ added testimonial grid (#208)
    * ✨ added testimonial grid
    
    * 🧹 fixed component not showing up
    
    * 🧹 styling changes to testimonial grid
    
    * 🧹 added discord button in main page

--- src/showcase/ui-group/ComponentCards.tsx
+++ src/showcase/ui-group/ComponentCards.tsx
@@ -8,73 +8,56 @@ import AnimatedLogoCloud from '../components/logo-cloud/AnimatedLogoCloud'
 
 const data = [
   {
-    id: 1,
     title: 'Button',
     component: <HeartbeatButton />,
     link: '/components/button',
   },
   {
-    id: 2,
     title: 'Features',
     image: '/images/ui/features.svg',
     link: '/components/features',
   },
   {
-    id: 3,
     title: 'Pricing',
     image: '/images/ui/pricing.svg',
     link: '/components/pricing',
   },
   {
-    id: 4,
     title: 'Loaders',
     component: <OrbitingLoader />,
     link: '/components/loaders',
   },
   {
-    id: 5,
+    title: 'Testimonial',
+    image: '/images/ui/testimonial.svg',
+    link: '/components/testimonial',
+  },
+  {
     title: 'Text',
     component: <TextTicker />,
     link: '/components/text',
   },
   {
-    id: 6,
     title: 'Footer',
     image: '/images/ui/footer.svg',
     link: '/components/footer',
   },
   {
-    id: 7,
     title: 'Input',
     image: '/images/ui/input.png',
     link: '/components/input',
   },
   {
-    id: 8,
     title: 'Tab',
     image: '/images/ui/tabs.png',
     link: '/components/tabs',
   },
-  // {
-  //   id: 1,
-  //   title: 'Badges',
-  //   image: '/images/ui/badges.png',
-  //   link: '/components/badges',
-  // },
-  // {
-  //   id: 9,
-  //   title: 'Breadcrumb',
-  //   image: '/images/ui/breadcrumb.png',
-  //   link: '/components/breadcrumb',
-  // },
   {
-    id: 10,
     title: 'Toggle',
     component: <SimpleToggle />,
     link: '/components/toggle',
   },
   {
-    id: 11,
     title: 'Logo Cloud',
     component: <AnimatedLogoCloud />,
     link: '/components/logo-cloud',
@@ -85,9 +68,9 @@ const ComponentCards = () => {
   return (
     <div>
       <div className="grid grid-cols-2 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {data.map((item) => (
-          <Link href={item.link} key={item.id} className="no-underline">
-            <div className="overflow group rounded-xl border border-white ring-1 ring-zinc-200 transition-all ease-in-out hover:cursor-pointer dark:border-gray-600 dark:ring-gray-600">
+        {data.map((item, index) => (
+          <Link href={item.link} key={index} className="no-underline">
+            <div className="overflow group rounded-xl border border-white ring-1 ring-zinc-200 transition-all ease-in-out hover:cursor-pointer">
               <div className="flex h-[9rem] items-center justify-center overflow-hidden rounded-t-xl border-b bg-gray-50 text-xs text-gray-400 transition-all ease-in-out group-hover:bg-gray-100 md:h-[12rem]">
                 {item.image ? (
                   <Image
