1ed0bebe6e7e42076cdcae789fcefd97242680ef
ansub <khanansub3@gmail.com>
2024-05-29 19:17:01 +0400

    🧹 dark mode styling fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,13 +56,13 @@ const Home = () => {
             <span className="max-w-lg text-3xl font-bold tracking-tight md:max-w-2xl md:text-5xl">
               Stop coding from scratch. Build faster. Launch sooner.
             </span>
-            <div className="text-gray-600">
+            <div className="text-gray-600 dark:text-gray-300">
               Free-to-use UI elements designed for rapid development.
             </div>
           </div>
           <div className="flex items-center space-x-4">
             <Link href="/components">
-              <Button className="bg-red-500 hover:bg-red-500/90">
+              <Button className="bg-red-500 hover:bg-red-500/90 dark:bg-red-500 dark:text-white dark:hover:bg-red-500/90">
                 Get Started
               </Button>
             </Link>
