3368850c74bc2101bff317fcee1a77f2a61b3907
ethan <epollack31@gmail.com>
2024-06-02 11:08:22 -0700

    DiscordButton -> TemplatesButton / updated changed the button from onClick router navigation to Link navigation / updated href in link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,34 +11,27 @@ import { motion } from 'framer-motion'
 import { ChevronRight } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
-import { useRouter } from 'next/navigation'
 
-const DiscordButton = () => {
-  const router = useRouter()
-
+const TemplatesButton = () => {
   return (
-    <motion.button
-      className="relative mb-8 inline-flex overflow-hidden rounded-full p-[1px] ring-1 ring-inset ring-blue-700/10"
-      initial={{ opacity: 0, scale: 0.5 }}
-      animate={{ opacity: 1, scale: 1 }}
-      transition={{ duration: 0.5 }}
-      whileHover={{ scale: 1.1 }}
-      whileTap={{ scale: 0.9 }}
-      onClick={() => {
-        // window.open('https://cal.com/ansub/15')
-        // window.open('https://discord.com/invite/P8GXYyH3ZU')
-        router.push('/templates/minimal-portfolio')
-        // posthog.capture('meeting_button_clicked')
-      }}
-    >
-      <span className="absolute inset-[-1000%] animate-discord-button bg-[conic-gradient(from_calc(var(--discord-button-angle)+60deg)_at_calc(50%+var(--discord-button-x))_50%,transparent_50%,#fb3a5d_98%,transparent_100%)]"></span>
-      <span className="inline-flex items-center gap-2 rounded-full bg-red-100 px-3 py-1 text-[12px] font-medium uppercase text-red-500 backdrop-blur">
-        <span>
-          Get Portfolio Template
-          <ChevronRight className="inline-block h-4 w-4 text-red-400" />
+    <Link href="/templates" passHref>
+      <motion.a
+        className="relative mb-8 inline-flex overflow-hidden rounded-full p-[1px] ring-1 ring-inset ring-blue-700/10"
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.5 }}
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+      >
+        <span className="absolute inset-[-1000%] animate-discord-button bg-[conic-gradient(from_calc(var(--discord-button-angle)+60deg)_at_calc(50%+var(--discord-button-x))_50%,transparent_50%,#fb3a5d_98%,transparent_100%)]"></span>
+        <span className="inline-flex items-center gap-2 rounded-full bg-red-100 px-3 py-1 text-[12px] font-medium uppercase text-red-500 backdrop-blur">
+          <span>
+            Get Portfolio Template
+            <ChevronRight className="inline-block h-4 w-4 text-red-400" />
+          </span>
         </span>
-      </span>
-    </motion.button>
+      </motion.a>
+    </Link>
   )
 }
 
@@ -49,7 +42,7 @@ const Home = () => {
         <Header />
         <div className="my-[8rem] flex h-full flex-col items-center justify-center gap-4 px-3">
           <div className="flex w-full flex-col items-center justify-center gap-2">
-            <DiscordButton />
+            <TemplatesButton />
             <Image
               src="/images/syntaxUI.svg"
               alt="syntaxUI"
