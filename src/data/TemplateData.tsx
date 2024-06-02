e4320bd3078aa0b179515879f6557686538f39ef
ansub <khanansub3@gmail.com>
2024-06-02 14:13:01 +0400

    ✨ added slug for template

---
+++ src/data/TemplateData.tsx
@@ -0,0 +1,23 @@
+export const TemplateData = [
+  {
+    name: 'Minimal Portfolio',
+    image: '/images/templates/minimal-portfolio.png',
+    id: 'minimal-portfolio',
+    price: 19,
+    originalPrice: 49,
+  },
+  {
+    name: 'Modern Business',
+    image: '/images/templates/minimal-portfolio.png',
+    id: 'modern-business',
+    price: 29,
+    originalPrice: 59,
+  },
+  {
+    name: 'Creative Agency',
+    image: '/images/templates/minimal-portfolio.png',
+    id: 'creative-agency',
+    price: 39,
+    originalPrice: 69,
+  },
+]
