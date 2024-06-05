233076244e8ae489bd9e1860f74ab7388671cd32
ansub <khanansub3@gmail.com>
2024-06-05 15:00:12 +0400

    🧹 decreased text-size for TextGradient

--- src/showcase/components/text/TextGradient.tsx
+++ src/showcase/components/text/TextGradient.tsx
@@ -1,6 +1,6 @@
 const TextGradient = () => {
   return (
-    <h1 className="animate-textGradient bg-gradient-to-r from-red-500 via-yellow-500 to-green-500 bg-clip-text text-3xl font-semibold text-transparent">
+    <h1 className="animate-textGradient bg-gradient-to-r from-red-500 via-yellow-500 to-green-500 bg-clip-text text-2xl font-semibold text-transparent">
       SyntaxUI
     </h1>
   )
