3e9378b1b30d4dcf502675c47bc471137305a182
ansub <khanansub3@gmail.com>
2024-06-06 14:10:49 +0400

    🧹 fixed params of CrossPattern

--- src/showcase/effects/CrossPatternBackground.tsx
+++ src/showcase/effects/CrossPatternBackground.tsx
@@ -11,10 +11,9 @@ interface CrossPatternBackgroundProps {
 }
 
 export const CrossPatternBackground: React.FC<CrossPatternBackgroundProps> = ({
-  crossSize = 1,
   crossColor = '#a1a1a1',
   backgroundColor = 'transparent',
-  gap = 40,
+  crossSize = 40,
   className,
   fade = true,
   style,
@@ -31,7 +30,7 @@ export const CrossPatternBackground: React.FC<CrossPatternBackgroundProps> = ({
 
   const backgroundStyle: React.CSSProperties = {
     backgroundColor,
-    backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='${gap}' height='${gap}' viewBox='0 0 40 40'%3E%3Cg fill-rule='evenodd'%3E%3Cg fill='${encodedCrossColor}' fill-opacity='0.4'%3E%3Cpath d='M0 38.59l2.83-2.83 1.41 1.41L1.41 40H0v-1.41zM0 1.4l2.83 2.83 1.41-1.41L1.41 0H0v1.41zM38.59 40l-2.83-2.83 1.41-1.41L40 38.59V40h-1.41zM40 1.41l-2.83 2.83-1.41-1.41L38.59 0H40v1.41zM20 18.6l2.83-2.83 1.41 1.41L21.41 20l2.83 2.83-1.41 1.41L20 21.41l-2.83 2.83-1.41-1.41L18.59 20l-2.83-2.83 1.41-1.41L20 18.59z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`,
+    backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='${crossSize}' height='${crossSize}' viewBox='0 0 40 40'%3E%3Cg fill-rule='evenodd'%3E%3Cg fill='${encodedCrossColor}' fill-opacity='0.4'%3E%3Cpath d='M0 38.59l2.83-2.83 1.41 1.41L1.41 40H0v-1.41zM0 1.4l2.83 2.83 1.41-1.41L1.41 0H0v1.41zM38.59 40l-2.83-2.83 1.41-1.41L40 38.59V40h-1.41zM40 1.41l-2.83 2.83-1.41-1.41L38.59 0H40v1.41zM20 18.6l2.83-2.83 1.41 1.41L21.41 20l2.83 2.83-1.41 1.41L20 21.41l-2.83 2.83-1.41-1.41L18.59 20l-2.83-2.83 1.41-1.41L20 18.59z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`,
     ...maskStyle,
     ...style,
   }
