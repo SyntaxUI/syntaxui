cbf57d8fd31388f6105531b49f205008d94f4c7b
ansub <khanansub3@gmail.com>
2024-06-06 14:07:23 +0400

    ✨ add cross pattern

--- src/showcase/ui-group/BackgroundCards.tsx
+++ src/showcase/ui-group/BackgroundCards.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import DotPatternBackground from '../effects/BackgroundDots'
 import BackgroundGrid from '../effects/BackgroundGrid'
 import Card from './Card'
+import CrossPatternBackground from '../effects/CrossPatternBackground'
 
 const data = [
   {
@@ -22,6 +23,19 @@ const data = [
     link: '/effects/background/dots',
     component: (
       <DotPatternBackground
+        className="relative z-0"
+        dotColor="#a1a1a1"
+        size="25px"
+        strokeWidth="3px"
+        fade={false}
+      />
+    ),
+  },
+  {
+    title: 'Cross',
+    link: '/effects/background/cross',
+    component: (
+      <CrossPatternBackground
         className="relative z-0"
         dotColor="#a1a1a1"
         size="25px"
