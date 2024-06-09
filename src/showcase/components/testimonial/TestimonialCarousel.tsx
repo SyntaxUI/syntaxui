a25a43b898fdcc490d4dd9e70bfc7e3d9b8c30ef
ansub <khanansub3@gmail.com>
2024-06-09 14:27:41 +0400

    ✨ final styling and animation changes

--- src/showcase/components/testimonial/TestimonialCarousel.tsx
+++ src/showcase/components/testimonial/TestimonialCarousel.tsx
@@ -1,20 +1,34 @@
 'use client'
 
-import { useState, useEffect } from 'react'
-import { motion, AnimatePresence, delay } from 'framer-motion'
+import { AnimatePresence, motion } from 'framer-motion'
+import { useEffect, useState } from 'react'
 
 const testimonials = [
   {
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita voluptas culpa .',
-    author: 'Judith Black',
-    title: 'CEO of Workcation',
+    text: 'First testimonial goes here. Praising your product or service and expressing satisfaction.',
+    author: 'Ansub',
+    image:
+      'https://res.cloudinary.com/dfhp33ufc/image/upload/v1715276558/logos/t2awrrfzdvmg1chnzyfr.svg',
   },
   {
     text: 'Another testimonial goes here. Praising your product or service and expressing satisfaction.',
-    author: 'John Doe',
-    title: 'Founder of ABC Inc.',
+    author: 'Lex Collins',
+
+    image:
+      'https://res.cloudinary.com/dfhp33ufc/image/upload/v1715276558/logos/pmblusboe7vkw8vxdknx.svg',
   },
-  // Add more testimonials as needed
+  {
+    text: 'Third testimonial goes here. Praising your product or service and expressing satisfaction.',
+    author: 'Alex Jones',
+    image:
+      'https://res.cloudinary.com/dfhp33ufc/image/upload/v1715276558/logos/tyos2ayezryjskox3wzs.svg',
+  },
+  {
+    text: 'Fourth testimonial goes here. Praising your product or service and expressing satisfaction.',
+    author: 'John Doe',
+    image:
+      'https://res.cloudinary.com/dfhp33ufc/image/upload/v1715276560/logos/nymiivu48d5lywhf9rpf.svg',
+  },
 ]
 
 const TestimonialCarousel = () => {
@@ -25,23 +39,28 @@ const TestimonialCarousel = () => {
       setCurrentTestimonial(
         (prevTestimonial) => (prevTestimonial + 1) % testimonials.length,
       )
-    }, 5000)
+    }, 5000) // Change Time here
 
     return () => {
       clearInterval(intervalId)
     }
   }, [])
 
-  const { text, author, title } = testimonials[currentTestimonial]
+  const { text, author, image } = testimonials[currentTestimonial]
 
   const variants = {
-    initial: { opacity: 0, scale: 0.1, y: 100 },
-    animate: { opacity: 1, scale: 1, y: 0 },
-    exit: { opacity: 0, scale: 0.1, y: -100 },
+    initial: { opacity: 0, y: '100%', scale: 0.1 },
+    animate: { opacity: 1, y: 0, scale: 1 },
+    exit: { opacity: 0, y: '100%', scale: 0.1 },
   }
+  const dotVariants = {
+    active: { scale: 1.2, backgroundColor: '#3f3f46' },
+    inactive: { scale: 1, backgroundColor: '#D1D5DB' },
+  }
+
   return (
     <section className="relative overflow-hidden py-12 md:py-24">
-      <div className="mx-auto max-w-2xl lg:max-w-3xl">
+      <div className="w-full max-w-2xl">
         <AnimatePresence mode="popLayout">
           <motion.div
             key={currentTestimonial}
@@ -49,24 +68,35 @@ const TestimonialCarousel = () => {
             animate="animate"
             exit="exit"
             variants={variants}
+            className="flex w-full flex-col items-center justify-center"
             transition={{
               type: 'spring',
               stiffness: 200,
               damping: 20,
-              duration: 0.6,
-              delayChildren: 5,
+              duration: 0.5,
             }}
           >
-            <p className="text-center text-2xl font-semibold tracking-tight">
-              {text}
+            <img src={image} alt={author} className="m-0 h-24 w-24" />
+            <p className="m-0 text-center text-2xl font-semibold tracking-tight">
+              &quot;{text}&quot;
             </p>
-            <div className="mt-10 text-center">
-              <div className="mt-4 flex items-center justify-center space-x-3 text-base">
-                <div className="font-semibold text-gray-900">{author}</div>
-                <div className="text-gray-600">{title}</div>
+            <div className="mx-auto mt-5">
+              <div className="flex flex-col items-center justify-center space-x-3 text-base">
+                <div className="font-medium text-gray-600">{author}</div>
               </div>
             </div>
           </motion.div>
+          <div className="mt-8 flex justify-center">
+            {testimonials.map((_, index) => (
+              <motion.div
+                key={index}
+                className="mx-1 h-2 w-2 cursor-pointer rounded-full"
+                variants={dotVariants}
+                animate={index === currentTestimonial ? 'active' : 'inactive'}
+                onClick={() => setCurrentTestimonial(index)}
+              />
+            ))}
+          </div>
         </AnimatePresence>
       </div>
     </section>
