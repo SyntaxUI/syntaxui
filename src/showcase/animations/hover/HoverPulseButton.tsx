b9775edb91ba09ef7ff831d7ccab505aa15b4d05
Asin-Junior-Honore <135389589+Asin-Junior-Honore@users.noreply.github.com>
2024-06-24 11:13:43 +0100

    ✨added Pulse hover animation (#237)
    * Fix issue with hover animations
    
    * swapped pure CSS classes for inline TailwindCSS classes
    
    * restored the existing content in tailwind.css file
    
    * modifed previous changes for consistency
    
    * feat: add meta data for hover-pulse animation

---
+++ src/showcase/animations/hover/HoverPulseButton.tsx
@@ -0,0 +1,7 @@
+export const HoverPulseButton = () => {
+  return (
+    <button className="rounded-lg bg-red-500 px-6 py-2 font-semibold text-white no-underline group-hover:animate-hover-pulse">
+      Hover Me
+    </button>
+  )
+}
