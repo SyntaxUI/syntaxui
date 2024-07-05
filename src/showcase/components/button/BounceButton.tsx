788be85dbc66ba2e429310cf9dd6eddfd8e111f2
ansub <khanansub3@gmail.com>
2024-07-05 14:06:41 +0400

    🧹 changed roundedness of the button

--- src/showcase/components/button/BounceButton.tsx
+++ src/showcase/components/button/BounceButton.tsx
@@ -7,7 +7,7 @@ const BounceButton = () => {
     <div>
       <motion.button
         whileHover={{ scale: 1.1 }}
-        className="rounded-full bg-red-500 px-6 py-2 text-sm font-medium text-white"
+        className="rounded-xl bg-red-500 px-6 py-2 text-sm font-medium text-white"
         transition={{ type: 'spring', stiffness: 400, damping: 10 }}
       >
         SyntaxUI
